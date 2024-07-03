f123d5a72f11129dd3a8d387085361054338b54a
Osemu <vincentaimiosior@gmail.com>
2024-07-03 20:09:45 +0000

    first commit

---
+++ src/main/java/com/predmod/app/PredModApplication.java
@@ -0,0 +1,13 @@
+package com.predmod.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredModApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredModApplication.class, args);
+	}
+
+}
